f794c5f3d01211c85b6b2c1c40ea7a533038c2b3
andrsharov <andrsharov@gmail.com>
2025-04-30 20:52:43 +0300

    Fix WARNING "'orm_mode' has been renamed to 'from_attributes'"

--- app/schemas/booking.py
+++ app/schemas/booking.py
@@ -28,7 +28,6 @@ class BookingGetResponse(BookingSchema):
     status: int
 
     class Config:
-        orm_mode = True
         json_schema_extra = {
             "example": {
                 "id": 1,
