eec0b9e8344b06328d4ba16b4bacdb72c6d37007
andrsharov <andrsharov@gmail.com>
2025-05-02 13:50:46 +0300

    FIX pylint WARNINGS

--- app/schemas/books.py
+++ app/schemas/books.py
@@ -1,11 +1,14 @@
+"""pydantic schemas for /books"""
 from pydantic import BaseModel
 
-class BookSchema(BaseModel):
+class BookSchema(BaseModel):  # pylint: disable=too-few-public-methods
+    """BookSchema Model"""
     book_title: str
     book_author: str
     book_year: int
 
-    class ConfigDict:
+    class ConfigDict:  # pylint: disable=too-few-public-methods
+        """Configuration Model"""
         json_schema_extra = {
             "example": {
                 "book_title": "Название книги",
