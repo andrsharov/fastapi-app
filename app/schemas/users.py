d64a6771ea2fdf2eb7299b17b2980588ba6fd7ee
andrsharov <andrsharov@gmail.com>
2025-05-02 14:16:32 +0300

    FIX pylint WARNINGS

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,11 +1,14 @@
+"""pydantic schemas for /users"""
 from pydantic import BaseModel
 
-class UserSchema(BaseModel):
+class UserSchema(BaseModel):  # pylint: disable=too-few-public-methods
+    """UserSchema Model"""
     user_name: str
     user_full_name: str
     user_bearer_access_token: str
 
-    class ConfigDict:
+    class ConfigDict:  # pylint: disable=too-few-public-methods
+        """Configuration Model"""
         json_schema_extra = {
             "example": {
                 "user_name": "Имя пользователя (Логин)",
